7d81885d939d098c3ba2011d82c8f819306ae46f
Manas Talukdar <manas.talukdar@gmail.com>
2023-04-08 19:40:35 -0700

    WIP

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ For Windows 10, the neovim's init.vim file is located in %userprofile%\AppData\L
 - <https://mattermost.com/blog/turning-neovim-into-a-full-fledged-code-editor-with-lua/>
 - <https://vonheikemen.github.io/devlog/tools/neovim-plugins-to-get-started/>
 - <https://medium.com/linux-with-michael/lazy-nvim-the-blazingly-fast-neovim-package-manager-19a7a952835c>
+- <https://dev.to/asyncedd/using-lazynvim-in-our-neovim-configuration-glg>
--- init.lua
+++ init.lua
@@ -6,32 +6,47 @@
 vim.g.mapleader = ","
 vim.g.localleader = "\\"
 
+require("settings")
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+ vim.fn.system({
+  "git",
+  "clone",
+  "--filter=blob:none",
+  "--single-branch",
+  "https://github.com/folke/lazy.nvim.git",
+  lazypath,
+ })
 end
-vim.opt.rtp:prepend(lazypath)
-require("lazy").setup(plugins, opts)
 
--- IMPORTS
-require('vars')      -- Variables
-require('opts')      -- Options
-require('keys')      -- Keymaps
-require('plug')      -- Plugins
+vim.opt.runtimepath:prepend(lazypath)
 
--- PLUGINS: Add this section
-require('nvim-tree').setup{}
--- Add the block below
-require('lualine').setup {
-  options = {
-    theme = 'dracula-nvim'
-  }
-}
-require('nvim-autopairs').setup{} -- Add this line
+require("lazy").setup("plugins", {
+ defaults = { lazy = true },
+ install = { colorscheme = { "tokyonight" } },
+ checker = { enabled = true },
+ performance = {
+  rtp = {
+   disabled_plugins = {
+    "gzip",
+    "matchit",
+    "matchparen",
+    "netrwPlugin",
+    "tarPlugin",
+    "tohtml",
+    "tutor",
+    "zipPlugin",
+   },
+  },
+ },
+ -- debug = true,
+})
+
+vim.api.nvim_create_autocmd("User", {
+ pattern = "VeryLazy",
+ callback = function()
+  require("keymaps")
+  require("commands")
+ end,
+})
